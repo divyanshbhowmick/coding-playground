5d6e86959f7f8f60ffab80d2f412e07a3d3d960a
divyanshbhowmick <divyansh.bhowmick3@gmail.com>
2021-06-06 03:09:11 +0530

    BitStrings differen approach!

--- src/code/cses/BitStrings.java
+++ src/code/cses/BitStrings.java
@@ -10,8 +10,10 @@ class BitStrings {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 
-		long INF = (long) 1e9 + 7;
-		long res = (long) Math.pow(2, n) % INF;
-		System.out.println(res);
+		long INF = (long) 1e9 + 7, sum = 1;
+		for (int i = 1; i <= n; i++){
+			sum = sum * 2 % INF; 
+		}
+		System.out.println(sum);
 	}
 }
