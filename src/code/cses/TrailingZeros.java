c7569fdda70cb57305f0d37cef03937565b36d70
divyanshbhowmick <divyansh.bhowmick3@gmail.com>
2021-06-06 15:37:13 +0530

    TrailingZeros!!

---
+++ src/code/cses/TrailingZeros.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class TrailingZeros {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        long sum = 0;
+        for (long i = 5; i <= n; i *= 5) {
+            sum += n/i;
+        }
+        System.out.println(sum);
+    }
+}
