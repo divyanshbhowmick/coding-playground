d023a50e62d78411bce793f28c5f3ce0eb85b7a9
divyanshbhowmick <divyansh.bhowmick3@gmail.com>
2021-06-09 23:34:48 +0530

    Tower of Hanoi!!

---
+++ src/code/cses/TowerOfHanoi.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class TowerOfHanoi {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println((1<<n) - 1);
+        toh(1, 2, 3, n);
+    }
+
+    public static void toh(int a, int b, int c, int n) {
+        if (n == 0) return;
+        toh(a, c, b, n - 1);
+        System.out.println(a + " " + c);
+        toh(b, a, c, n - 1);
+    }
+}
